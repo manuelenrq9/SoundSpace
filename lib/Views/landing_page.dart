6d2499a773a786d1e17140d7f43db5ce9cf8568e
manuelenrq9 <manuel.enrqt@gmail.com>
2024-02-21 19:31:40 -0400

    background gradient added

---
+++ lib/Views/landing_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(32),
+      ),
+    );
+  }
+}
