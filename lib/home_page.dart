f773982e71da946dbaa0f90af569b646586c561b
manuelenrq9 <manuel.enrqt@gmail.com>
2024-03-15 19:26:42 -0400

    Player Bar ready
    Player Bar ready

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,34 +3,72 @@ import 'package:audioplayers/audioplayers.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-  
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   @override
-  
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('AudioPlayer example'),
         ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            playSound();
-          },
-          child: Text("Play Sound"),
-        ), 
+        body:
+            Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 60,
+                color: Colors.black,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        playSound();
+                      },
+                      icon: Icon(
+                        Icons.play_arrow,
+                        color: Colors.white,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: (null),
+                      icon: Icon(
+                        Icons.pause,
+                        color: Colors.white,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: (null),
+                      icon: Icon(
+                        Icons.skip_previous,
+                        color: Colors.white,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: (null),
+                      icon: Icon(
+                        Icons.skip_next,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ]),
       ),
-     ),
-   );
+    );
   }
-  
+
   AudioPlayer player = AudioPlayer();
-  
+
   Future<void> playSound() async {
     String soundPath = "audio/ManInTheBox.mp3";
     await player.play(AssetSource(soundPath));
